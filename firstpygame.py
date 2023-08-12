ce2e12d93caa1f908cd516f02e212a3b037d865b
ExxonExxon <132812480+ExxonExxon@users.noreply.github.com>
2023-08-12 17:35:20 +1000

    Add files via upload

--- firstpygame.py
+++ firstpygame.py
@@ -1,29 +1,40 @@
 import pygame
 import random
 import pygame.font 
+import sys
 pygame.init()
 
+easter_egg = 0
+pygame.mixer.music.load("assets/background_song.mp3")
+pygame.mixer.music.play(-1)  # Play in an infinite loop
+score_taken_sound = pygame.mixer.Sound("assets/score_taken.wav")
+coin_sound = pygame.mixer.Sound("assets/coin.wav")
+font_path = "assets/font.ttf"
 score = 0
 screen_width = 800
 screen_height = 600
-
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 border_size = 5
-font = pygame.font.Font(None, 36)  # Choose font and size
-
-background_image = pygame.image.load("background.png")
+font = pygame.font.Font(font_path, 36)  # Choose font and size
+background_color = (0, 0, 0)
+button_color = (255, 255, 255)
+background_image = pygame.image.load("assets/background.png")
 background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
+
+BAT_SPEED = 3
+
 
 class Ghost(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.original_image = pygame.image.load("ghost.png")
-        self.original_image_mirror = pygame.image.load("ghost_mirror.png")
+        self.original_image = pygame.image.load("assets/ghost.png")
+        self.original_image_mirror = pygame.image.load("assets/ghost_mirror.png")
         self.scale_factor = 4
         self.image = pygame.transform.scale(self.original_image, (16 * self.scale_factor, 16 * self.scale_factor))
         self.rect = self.image.get_rect()
         self.rect.center = (100, screen_height // 1.1)
-        self.speed = 0.51
+        self.speed = 6
 
     def update(self):
         keys = pygame.key.get_pressed()
@@ -41,32 +52,56 @@ class Ghost(pygame.sprite.Sprite):
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             if self.rect.bottom + self.speed <= screen_height - border_size:
                 self.rect.y += self.speed
+         
+
+def start_game():
+    global game_started
+    game_started = True
+
+class Bat(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("assets/bat.gif")  # Load bat image
+        self.image = pygame.transform.scale(self.image, (64, 32))
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(0, screen_width), random.randint(0, screen_height))
+        self.speed = BAT_SPEED  # Add random speed to each bat
+        self.direction_x = random.choice([-1, 1])  # Randomly choose left or right direction
+        self.direction_y = random.choice([-1, 1])  # Randomly choose up or down direction
+
+    def update(self):
+        self.rect.x += self.speed * self.direction_x
+        self.rect.y += self.speed * self.direction_y
+        
+        # Check for collision with screen edges and change direction
+        if self.rect.left < 0 or self.rect.right > screen_width:
+            self.direction_x *= -1  # Change x direction when hitting screen edge
+        if self.rect.top < 0 or self.rect.bottom > screen_height:
+            self.direction_y *= -1  # Change y direction when hitting screen edge
+            
+        # Ensure the bat stays within the screen bounds
+        self.rect.x = max(0, min(self.rect.x, screen_width - self.rect.width))
+        self.rect.y = max(0, min(self.rect.y, screen_height - self.rect.height))
+
+bats = pygame.sprite.Group()
+for _ in range(5):
+    bat = Bat()
+    bats.add(bat)
 
 class Gravestone(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("gravestone.png")
+        self.image = pygame.image.load("assets/gravestone.png")
         self.image = pygame.transform.scale(self.image, (32, 32))
         self.rect = self.image.get_rect()
         self.rect.center = (random.randint(0, screen_width), random.randint(0, screen_height))
 
-class AIGhost(pygame.sprite.Sprite):
-    def __init__(self, target):
-        super().__init__()
-        self.original_image = pygame.image.load("ai_ghost.png")
-        self.scale_factor = 4
-        self.image = pygame.transform.scale(self.original_image, (16 * self.scale_factor, 16 * self.scale_factor))
-        self.rect = self.image.get_rect()
-        self.target = target  # The target is the player-controlled ghost
-        # Additional attributes for AI logic
-
 gravestones = pygame.sprite.Group()
-for _ in range(10):
+for _ in range(3):
     gravestone = Gravestone()
     gravestones.add(gravestone)
 
 my_sprite = Ghost()
-ai_ghost = AIGhost(Ghost)
 
 running = True
 while running:
@@ -74,7 +109,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    pygame.display.update()
     my_sprite.update()
+
 
     screen.blit(background_image, (0, 0))
     screen.blit(my_sprite.image, my_sprite.rect.topleft)
@@ -87,21 +124,53 @@ while running:
     for _ in gravestones_hit:
         new_gravestone = Gravestone()
         score += 1
+        BAT_SPEED += 0.01  # Increase bat speed
+        coin_sound.play()  # Play the coin sound effect        
         gravestones.add(new_gravestone)
     
-    # Check for collision with AI ghost
-    if pygame.sprite.collide_rect(Ghost, ai_ghost):
-        # Reset game elements and restart the game
-        score = 0
-        Ghost.rect.center = (100, screen_height // 1.1)
-        ai_ghost.rect.center = (random.randint(0, screen_width), random.randint(0, screen_height))
+    for bat in bats:
+        bat.speed = BAT_SPEED
+        bat.update()
+        screen.blit(bat.image, bat.rect.topleft)
+    
+    # Check for collision with bats
+        if my_sprite.rect.colliderect(bat.rect):
+            score -= 10
+            bat.rect.center = (random.randint(0, screen_width), random.randint(0, screen_height))  # Reposition the bat
+            score_taken_sound.play()  # Play the collision sound effect
+    
+    # Easter egg logic
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_t]:
+            easter_egg = 1
+        if easter_egg == 1 and keys[pygame.K_o]:
+            easter_egg = 2
+        if easter_egg == 2 and keys[pygame.K_m]:
+            easter_egg = 3
+        if easter_egg == 3 and keys[pygame.K_a]:
+            easter_egg = 4
+        if easter_egg == 4 and keys[pygame.K_s]:
+            easter_egg = 0
+            score += 150
+
 
     pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(0, 0, screen_width, border_size))
     pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(0, screen_height - border_size, screen_width, border_size))
     pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(0, 0, border_size, screen_height))
     pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(screen_width - border_size, 0, border_size, screen_height))
-    text = font.render(f"Score = {score}", True, (255, 255, 255))
+    if score < 0:
+        score = 0
+    if score < 33:
+        text = font.render(f"Score = {score}", True, (255, 255, 255))
+    if score >= 50:
+        text = font.render(f"Score = {score}★", True, (255, 255, 255))
+    if score >= 100:
+        text = font.render(f"Score = {score}★★", True, (255, 255, 255))
+    if score >= 200:
+        text = font.render(f"Score = {score}★★★", True, (255, 255, 255))
+
     screen.blit(text, (screen_width // 2 - text.get_width() // 2, 20))
+    clock.tick(60)  # Control the frame rate
     pygame.display.update()
 
 pygame.quit()
